72bfcb137f9cd206f18bc7b4c58e8d1e37793037
Samuel Ife <samuelife@hotmail.co.uk>
2023-06-03 23:28:19 +0100

    configurations

--- src/test/java/cucumberTest/runners/cucumber_serenity_runner.java
+++ src/test/java/cucumberTest/runners/cucumber_serenity_runner.java
@@ -1,11 +1,22 @@
 package cucumberTest.runners;
 
-import cucumber.api.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/set_OBS_stream_key.feature")
+@CucumberOptions(
+                 plugin = {"pretty"},
+                 features="src/test/resources/features",
+                 glue = {"cucumberTest"}
+)
 public class cucumber_serenity_runner {
+    @BeforeClass
+    public static void config() {
+        System.setProperty("webdriver.chrome.driver", "C:/OBS_Restream/src/main/resources/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+    }
 }
 
